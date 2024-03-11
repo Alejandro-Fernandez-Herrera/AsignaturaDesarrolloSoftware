36a13c7168c64719ec17150cdd4280157e1fe019
Jose Castellanos <jmcastellanos153@misena.edu.co>
2024-03-11 15:17:29 -0500

    conexion con postgresql y modelo de usuario

--- constructora/settings.py
+++ constructora/settings.py
@@ -37,11 +37,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
+    'rest_framework',
+    'constructora_app'
+    
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -75,8 +80,12 @@ WSGI_APPLICATION = 'constructora.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'constructora',
+        'USER': 'postgres',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
@@ -121,3 +130,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOWED_ORIGINS = [
+    
+]
---
+++ constructora_app/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+
+# Create your models here.
+
+class Usuario(models.Model):
+    TIPO_IDENTIFICACION = [
+        ('CC', 'Cédula de Ciudadanía'),
+        ('TI', 'Tarjeta de Identidad'),
+        ('CE', 'Cédula de Extranjería'),
+        ('PS', 'Pasaporte'),
+    ]
+    TIPO_USUARIO = [
+        ('AD', 'Administrador'),
+        ('US', 'Usuario Estándar'),
+        ('OP', 'Operador'),
+    ]
+    GENEROS = [
+        ('M', 'Masculino'),
+        ('F', 'Femenino'),
+        ('O', 'Otro'),
+    ]
+    fotografia = models.ImageField(upload_to='fotografias/', null=True, blank=True)
+    tipo_identificacion = models.CharField(max_length=2, choices=TIPO_IDENTIFICACION)
+    nro_identificacion = models.CharField(max_length=20, unique=True)
+    apellidos = models.CharField(max_length=100)
+    nombres = models.CharField(max_length=100)
+    tipo_usuario = models.CharField(max_length=2, choices=TIPO_USUARIO)
+    login = models.CharField(max_length=50, unique=True)
+    contraseña = models.CharField(max_length=50)
+    genero = models.CharField(max_length=1, choices=GENEROS)
+    direccion = models.CharField(max_length=200, null=True, blank=True)
+    celular = models.CharField(max_length=20, null=True, blank=True)
+    def __str__(self):
+        return f"{self.nombres} {self.apellidos}"
