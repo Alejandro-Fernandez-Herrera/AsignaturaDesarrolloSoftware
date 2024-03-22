1214c781e43b8eadca0ef5208fdd2cf30f0a7534
Jose Castellanos <jmcastellanos153@misena.edu.co>
2024-03-22 08:26:34 -0500

    estructura del front

--- constructoraApp/src/App.jsx
+++ constructoraApp/src/App.jsx
@@ -1,10 +1,9 @@
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import './App.css'
-import {UsersPage} from "./pages/UsersPage"
-import {UserFormPage} from "./pages/UserFormPage"
-import {LandingPage} from "./pages/LandingPage"
+
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Auth0Provider } from '@auth0/auth0-react';
+import { Dashboard } from "./components/Dashboard/Dashboard";
 
 function App() {
   const queryClient = new QueryClient();
@@ -21,9 +20,8 @@ function App() {
       <QueryClientProvider client={queryClient}>
         <BrowserRouter>
           <Routes>
-            <Route path="/" element={<LandingPage/>}/>
-            <Route path="/users" element={<UsersPage/>}/>
-            <Route path="/users-create" element={<UserFormPage/>}/>
+            <Route path="/" element={<Dashboard/>}/>
+            
 
           </Routes>
         </BrowserRouter>
